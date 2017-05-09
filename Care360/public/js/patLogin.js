7c74c5b3d350ab8a1b00da5ac92a4623289a47a2
manahil <manahil_1000@hotmail.com>
2017-05-09 20:24:26 +0500

    updated angular for PatLogin

--- Care360/public/js/patLogin.js
+++ Care360/public/js/patLogin.js
@@ -1,26 +1,32 @@
+
 (function() {
   var app = angular.module('myApp', ['ui.router']);
-  
+
   app.run(function($rootScope, $location, $state, LoginService) {
-    $rootScope.$on('$stateChangeStart', 
-      function(event, toState, toParams, fromState, fromParams){ 
+    $rootScope.$on('$stateChangeStart',
+      function(event, toState, toParams, fromState, fromParams){
           console.log('Changed state to: ' + toState);
       });
-    
+
       if(!LoginService.isAuthenticated()) {
         $state.transitionTo('login');
       }
   });
-  
+
   app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home');
-    
+
     $stateProvider
       .state('login', {
         url : '/login',
         templateUrl : 'login.html',
         controller : 'LoginController'
       })
+      .state('register',{
+        url: '/register',
+        templateUrl: 'register.html',
+         controller: 'RegisterController'
+       })
       .state('home', {
         url : '/home',
         templateUrl : 'home.html',
@@ -28,42 +34,65 @@
       });
   }]);
 
-  app.controller('LoginController', function($scope, $rootScope, $stateParams, $state, LoginService) {
+  app.controller('LoginController', function($http,$scope, $rootScope, $stateParams, $state, LoginService) {
     $rootScope.title = "Login";
-    
+
     $scope.formSubmit = function() {
-      if(LoginService.login($scope.username, $scope.password)) {
+      if(LoginService.login($http,$scope.username, $scope.password)) {
         $scope.error = '';
         $scope.username = '';
         $scope.password = '';
         $state.transitionTo('home');
       } else {
         $scope.error = "Incorrect username/password !";
-      }   
+      }
     };
-    
+
   });
-  
+
   app.controller('HomeController', function($scope, $rootScope, $stateParams, $state, LoginService) {
     $rootScope.title = "Login";
-    
+
   });
-  
+
+  app.controller('RegisterController', function($scope, $rootScope, $stateParams, $state, LoginService) {
+    $rootScope.title = "Register";
+    if(LoginService.register()) {
+
+        $state.transitionTo('home');
+      }
+
+
+  });
+ 
   app.factory('LoginService', function() {
     var admin = 'admin';
     var pass = 'pass';
     var isAuthenticated = false;
-    
     return {
-      login : function(username, password) {
-        isAuthenticated = username === admin && password === pass;
-        return isAuthenticated;
+      login : function($http,username, password) {
+          var req={
+            method:'POST',
+            url:'/docLog',
+            headers:{'Content-type':'application/json'},
+            data:{username:username,password:password}
+          }
+         $http(req).then(function(res){
+             console.log("succes wala")
+             console.log(res.data)
+         },function(req,res){
+             console.log("failure wala")
+             console.log(res)
+         })
       },
+      register: function() {
+        isAuthenticated = true
+    },
       isAuthenticated : function() {
         return isAuthenticated;
       }
     };
-    
+
   });
-  
+
 })();
